f176cf3b6ac779427a2bcd32cbc0255e54af3d07
lokeshparasuraman <lokeshparasu@gmail.com>
2025-08-16 15:17:20 +0530

    NearestZero solved2

--- src/Oops/graphs/NearestZero.java
+++ src/Oops/graphs/NearestZero.java
@@ -20,11 +20,12 @@ class NearestZero {
         int[][] ans = new int[m][n];
         boolean[][] visited = new boolean[m][n];
         Queue<Pair> q = new LinkedList<>();
+
         // Initialize the queue with zero locations
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 0) {
-                    q.offer(new Pair(i, j, 0));
+                    q.add(new Pair(i, j, 0));
                     visited[i][j] = true;
                 }
             }
@@ -59,6 +60,53 @@ class NearestZero {
         return ans;
     }
 
+        public static int[][] nearestZero2(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            int[][] ans = new int[m][n];
+            boolean[][] visited = new boolean[m][n];
+            Queue<int[]> q = new LinkedList<>();
+
+            // Single 2D direction array for up, right, down, left
+            int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+            // Initialize the queue with zero locations
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 0) {
+                        q.offer(new int[]{i, j, 0}); // [row, col, distance]
+                        visited[i][j] = true;
+                    }
+                }
+            }
+
+            // BFS to find the nearest zero
+            while (!q.isEmpty()) {
+                int[] curr = q.poll();
+                int r = curr[0];
+                int c = curr[1];
+                int d = curr[2];
+                ans[r][c] = d;
+
+                // Check all four directions using the single direction array
+                for (int[]arr:dir) {
+                    int nr = r + arr[0];
+                    int nc = c + arr[1];
+
+                    // Check if the new position is valid and not visited
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
+                        q.add(new int[]{nr, nc, d + 1});
+                        visited[nr][nc] = true;
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int m = scanner.nextInt(), n = scanner.nextInt();
@@ -69,6 +117,8 @@ class NearestZero {
                 grid[i][j] = scanner.nextInt();
             }
         }
+
+
         int[][] ans = nearestZero(grid);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
