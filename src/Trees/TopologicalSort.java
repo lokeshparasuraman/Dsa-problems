7b175ff7b969011ffedf91f0bf5d8db822be9f97
lokeshparasuraman <lokeshparasu@gmail.com>
2025-08-22 15:05:59 +0530

    TopologicalSort of DAG graph.

---
+++ src/Trees/TopologicalSort.java
@@ -0,0 +1,41 @@
+package Trees;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class TopologicalSort {
+    public static List<Integer> sort(int[][]graph){
+        List<Integer> list=new ArrayList<>();
+        int m= graph.length;
+        Stack<Integer> s=new Stack<>();
+        boolean [] visited=new boolean[m];
+        for (int i = 0; i < m; i++) {
+            if(!visited[i]){
+                dfs(i,graph,visited,s);
+            }
+        }
+        while(!s.empty()){
+            int num=s.peek();
+            list.add(num);
+            s.pop();
+        }
+        return list;
+    }
+    public static void dfs(int curr,int[][]grid,boolean[]visited,Stack<Integer> s){
+        visited[curr]=true;
+        for (int k:grid[curr]){
+            if(!visited[k]){
+                dfs(k,grid,visited,s);
+            }
+        }
+        s.push(curr);
+    }
+
+    static void main(String[] args) {
+        int[][]g={
+                {},{3},{3},{},{0,1},{0,2}
+        };
+        System.out.println(sort(g));
+        //Ans:[5,4,2,1,3,0]//
+    }
+}
