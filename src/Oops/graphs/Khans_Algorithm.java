b119d186a6b9f859b2d9d46df21b98f845eda642
lokeshparasuraman <lokeshparasu@gmail.com>
2025-08-23 20:10:12 +0530

    Khans_Algorithm for DAG

---
+++ src/Oops/graphs/Khans_Algorithm.java
@@ -0,0 +1,32 @@
+package Oops.graphs;
+import java.util.*;
+public class Khans_Algorithm {
+    public static List<Integer> sort(List<List<Integer>> graph) {
+        int n = graph.size();
+        int[] indegree = new int[n];
+        Queue<Integer> q = new ArrayDeque<>();
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int k : graph.get(i)) {
+                indegree[k] += 1;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+        while (!q.isEmpty()) {
+            int front = q.poll();
+            ans.add(front);
+            for (int k : graph.get(front)) {
+                indegree[k] -= 1;
+                if (indegree[k] == 0) {
+                    q.add(k);
+                }
+            }
+        }
+        return ans;
+    }
+
+}
